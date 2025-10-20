b3245ac2dda82ee1261ba1d0660ae0afc731da20
realdev81 <takumihasegawa54321@gmail.com>
2025-10-19 22:40:54 -0700

    Fix: route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<div>Login</div>}/>
+        <Route path="/login" element={<div>Login</div>}/>
         <Route path='*' element={<div>404 Page not found<Link to="/">Go to Home</Link></div>} />
       </Routes>
     </BrowserRouter>
